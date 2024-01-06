3e56eda172485c5d731dff3227dc647b36d6cb99
Your Name <bridcallaghan56@gmail.com>
2024-01-06 21:19:24 +0000

    db creation probs

--- YogaStudio/App/YogaDAO/models.py
+++ YogaStudio/App/YogaDAO/models.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from flask_bcrypt import Bcrypt
-from base import Base
 
 from . import db 
 
