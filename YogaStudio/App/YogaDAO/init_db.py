7ba8c4501b0f149796e47f4a42cea5931693d295
Your Name <bridcallaghan56@gmail.com>
2024-01-07 12:54:21 +0000

    trying to get the relative pathsright

--- YogaStudio/App/YogaDAO/init_db.py
+++ YogaStudio/App/YogaDAO/init_db.py
@@ -1,12 +1,7 @@
-import sys
-# Add YogaStudio directory to sys.path
-sys.path.insert(0, '/home/G00411262/data-representation-Project/YogaStudio')
-
-from YogaDAO import db
-from models import User, Classes, Bookings  # Adjust the import path based on your project structure
+from . import db
+from .models import User, Classes, Bookings
 
 def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
-
