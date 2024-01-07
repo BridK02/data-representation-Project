311d9f4f43c80bd372dbdf8a71fd7728a34bcb79
Your Name <bridcallaghan56@gmail.com>
2024-01-07 20:12:21 +0000

    maybe

--- YogaStudio/App/YogaDAO/init_db.py
+++ YogaStudio/App/YogaDAO/init_db.py
@@ -1,11 +1,7 @@
-#import sys
-#sys.path.insert(0, '/home/G00411262/data-representation-Project/YogaStudio')
-#from . import db
-#from .models import User, Classes, Bookings
-
 import sys
-from YogaStudio.App import create_app, db
-from YogaStudio.App.YogaDAO.models import User, Classes, Bookings
+sys.path.insert(0, '/home/G00411262/data-representation-Project/YogaStudio')
+from . import db
+from .models import User, Classes, Bookings
 
 def init_db(app):
     db.init_app(app)
