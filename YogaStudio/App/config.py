a6710ad3f5f88427868ed1df7d0bee2733777865
Your Name <bridcallaghan56@gmail.com>
2024-01-07 14:40:18 +0000

    trying to get the pamysqp to wrk right

--- YogaStudio/App/config.py
+++ YogaStudio/App/config.py
@@ -4,4 +4,6 @@ class Config:
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'Myterypassword'
-    
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://user:password@host/database'
+   
+ 
--- YogaStudio/App/create_app.py
+++ YogaStudio/App/create_app.py
@@ -1,14 +1,15 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from YogaStudio.App.config import Config
-from .YogaDAO import db
 
 db = SQLAlchemy()
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__, static_url_path='/static', template_folder='templates')
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
-    db.init_app(app)
+    db.init_app(app)  # Initialize SQLAlchemy with the app
+
+    # Other configurations and extensions
 
     return app
+
