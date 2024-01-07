23c2b80a4c220449f90b2c008ae91dcda2e6e30d
Your Name <bridcallaghan56@gmail.com>
2024-01-07 12:28:50 +0000

    changes to app.py to connect to pamysql

--- YogaStudio/App/YogaDAO/init_db.py
+++ YogaStudio/App/YogaDAO/init_db.py
@@ -2,8 +2,8 @@ import sys
 # Add YogaStudio directory to sys.path
 sys.path.insert(0, '/home/G00411262/data-representation-Project/YogaStudio')
 
-from YogaStudio.App.YogaDAO import db
-from models import User, Classes, Bookings
+from /YogaDAO import db
+from /models import User, Classes, Bookings
 
 app = create_app()
 
